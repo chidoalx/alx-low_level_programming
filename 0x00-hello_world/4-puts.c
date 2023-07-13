b624d22f418206c0efcf2cc49e708256f69b20fa
chidoalx <elfagrouch9@gmail.com>
2023-07-13 12:53:45 -0700

    0x00-hello_world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main - this is my first line on c programe
+* return - 0 for positive valio 1 for negative valio
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
+}
