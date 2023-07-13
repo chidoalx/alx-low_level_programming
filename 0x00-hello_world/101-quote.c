278fce1212f6f40cf412563ad33df9a243bcadbb
chidoalx <elfagrouch9@gmail.com>
2023-07-13 15:35:30 -0700

    0x00-hello_world

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+/**
+ * main - the main function
+ * write - function to print
+ * Return: Description of the returned value
+ */
+int main(void)
+{
+write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+return (1);
+}
