d9abadffed77bb2dfbe5d104e0f90e19c6fb0731
chidoalx <elfagrouch9@gmail.com>
2023-07-14 13:04:46 -0700

    0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - the main function
+ * putchar - function to print
+ * for - functions of addition
+ * Return: Description of the returned value
+ */
+int main(void)
+{
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+putchar(letter);
+}
+putchar('\n');
+return (0);
+}
