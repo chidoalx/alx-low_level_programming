5630e4676fd4a75e44e9867b2f184c94100a1b1c
chidoalx <elfagrouch9@gmail.com>
2023-07-20 15:39:02 -0700

    0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *more_numbers - prints the numbers, from 0 to 9
+ *Return: product.
+ */
+void more_numbers(void)
+{
+int a;
+int b;
+for (b = 0; b <= 10; b++)
+{
+for (a = 0; a <= 14; a++)
+{
+printf("%d", a);
+}
+printf("\n");
+}
+}
