cd04c00ae965a82253a654d103fe2e4c96aea44c
chidoalx <elfagrouch9@gmail.com>
2023-07-20 16:59:22 -0700

    0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,22 +1,21 @@
 #include "main.h"
 #include <stdio.h>
 /**
- *print_line - prints the line
- *Return: product.
- */
+ * print_line - print a straight line
+ *
+ * @n: is the number of times the _ character
+ *     should be printed
+*/
 void print_line(int n)
 {
-int i ;
-if (n > 0)
-{
-for (i = 0; i <= n; i++)
-{
-printf("_");
-}
-printf("\n");
-}
-else
-{
-printf("\n");
-}
+	int lnChr;
+
+	if (n <= 0)
+		printf("\n");
+	else
+	{
+		for (lnChr = 1; lnChr <= n; ++lnChr)
+			printf("_");
+		printf("\n");
+	}
 }
