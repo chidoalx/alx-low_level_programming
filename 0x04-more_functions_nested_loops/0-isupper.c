329a7f5f15bebed5bb8e8b990768a7b9ed0477d8
chidoalx <elfagrouch9@gmail.com>
2023-07-20 11:53:51 -0700

    0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * _isupper - the main function
+ * puts - function to print
+ * Return: Description of the returned value
+ */
+int _isupper(int c) {
+    return c >= 'A' && c <= 'Z' ? 1 : 0;
+}
