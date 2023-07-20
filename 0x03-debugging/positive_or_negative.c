7bc5a1969b020cdfdc5eb226083d1dedf731bc94
chidoalx <elfagrouch9@gmail.com>
2023-07-19 19:21:45 -0700

    0x03-debugging

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,30 +1,28 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
-
+#include "0-main.c"
+#include "main.h"
 /**
- * main - Entry point
- *
- * Description: print value of n satus; zero, positive or negative
- *
- * Return: Always 0 (Success)
-*/
-
-int main(void)
+ * positive_or_negative - myfunction
+ * printf - function to print
+ * if_else - functions of comparison
+ * Return: Description of the returned value
+ */
+int main ()
 {
-	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-
-	/*my code*/
-	if (n > 0)
-		printf("%i is positive\n", n);
-	else if (n == 0)
-		printf("%i is zero\n", n);
-	else
-		printf("%i is negative\n", n);
-
-	return (0);
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+{
+printf("%i is positive\n", n);
+}
+else if (n == 0)
+{printf("%i is zero\n", n);
+}else
+{
+printf("%i is negative\n", n);
+}
+return (0);
 }
