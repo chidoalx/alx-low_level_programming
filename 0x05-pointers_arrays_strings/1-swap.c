572a51565bd8d501c5a8b081f2aeefce8d8c57ee
chidoalx <elfagrouch9@gmail.com>
2023-07-24 12:20:47 -0700

    0x05-pointers_arrays_strings

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,10 +1,12 @@
 /**
- * swap_int - swaps to pointers
- * @n: int parameter
+ * swap_int - swaps the values of two integers
+ *		using two input parameters
+ *
+ * @a: input parameter 1
+ * @b: input parameter 2
  *
  * Return: Nothing
- */
-
+*/
 void swap_int(int *a, int *b)
 {
 int swap;
