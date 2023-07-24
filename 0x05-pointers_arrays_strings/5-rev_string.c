b374b076319a40777aa9526fa6e1a3ef5fd5c0b3
chidoalx <elfagrouch9@gmail.com>
2023-07-24 13:38:00 -0700

    0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * rev_string - reverses a string
+ *
+ * @s: string parameter input
+ *
+ * Return: Nothing
+*/
+void rev_string(char *s)
+{
+int length = strlen(s);
+int start = 0;
+int end = length - 1;
+while (start < end)
+{
+char temp = s[start];
+s[start] = s[end];
+s[end] = temp;
+start++;
+end--;
+}
+}
