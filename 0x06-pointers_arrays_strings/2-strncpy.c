2fc4d863312a0ad56b1be7785fb7e3be3b7f2671
chidoalx <elfagrouch9@gmail.com>
2023-07-26 13:28:40 -0700

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,16 +1,24 @@
 #include "main.h"
+/**
+ * _strncpy - A function that copies a string.
+ * @dest: pointer to destination input buffer
+ * @src: pointer to source input buffer
+ * @n: bytes of @src
+ * Return: @dest
+*/
 char *_strncpy(char *dest, char *src, int n)
 {
 char *dest_ptr = dest;
 /* Copy at most n characters from src to dest*/
-while (*src != '\0' && n > 0) {
+while (*src != '\0' && n > 0)
+{
 *dest_ptr = *src;
 dest_ptr++;
 src++;
 n--;
 }
-/* Pad the remaining characters with null bytes if n is greater than the length of src*/
-while (n > 0) {
+while (n > 0)
+{
 *dest_ptr = '\0';
 dest_ptr++;
 n--;
