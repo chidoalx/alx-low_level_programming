d8b7286a4e152df0b7fbe765426a12c2afcf1369
chidoalx <elfagrouch9@gmail.com>
2023-07-26 13:05:23 -0700

    0x06-pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+char *_strncpy(char *dest, char *src, int n)
+{
+char *dest_ptr = dest;
+/* Copy at most n characters from src to dest*/
+while (*src != '\0' && n > 0) {
+*dest_ptr = *src;
+dest_ptr++;
+src++;
+n--;
+}
+/* Pad the remaining characters with null bytes if n is greater than the length of src*/
+while (n > 0) {
+*dest_ptr = '\0';
+dest_ptr++;
+n--;
+}
+return dest;
+}
